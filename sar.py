f56eae91ffec76d33275816acc4a39a916c64973
Nathan Schneider <nathan.schneider@sap.com>
2017-10-05 13:30:47 -0300

    Create python script to perform the same function. Much better

---
+++ sar.py
@@ -0,0 +1,36 @@
+import os
+import sys 
+import re
+# The top argument for walk
+topdir = sys.argv[1]
+old_pattern = sys.argv[2].lower()
+new_pattern = sys.argv[3].lower()
+old_pattern_for_file_system = old_pattern.replace('/', '#')
+new_pattern_for_file_system = new_pattern.replace('/', '#')
+
+old_p_regex = re.compile(re.escape(old_pattern), re.IGNORECASE)
+
+for dirpath, dirnames, files in os.walk(topdir, False):
+  if '.git' in dirpath:
+    continue
+  for name in files:
+    if old_pattern in name: #rename files in current dir if necessary
+      new_file_name = name.replace(old_pattern_for_file_system, new_pattern_for_file_system)
+      os.rename(os.path.join(dirpath, name), os.path.join(dirpath, new_file_name))
+      name = new_file_name
+    
+    f = open(os.path.join(dirpath, name), 'r')
+    filedata = f.read()
+    f.close()
+
+    new_file_data = old_p_regex.sub(new_pattern, filedata)
+
+    f = open(os.path.join(dirpath, name), 'w')
+    f.write(new_file_data)
+    f.close()
+    
+  if old_pattern in dirpath: #rename current dir if necessary
+    new_dir_path = dirpath.replace(old_pattern_for_file_system, new_pattern_for_file_system)
+    os.rename(dirpath, new_dir_path)
+
+    
