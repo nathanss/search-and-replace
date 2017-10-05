bd48901a9fa62dcec5e9ce0392df3e06800d9144
Nathan Schneider <nathan.schneider@sap.com>
2017-10-05 14:36:29 -0300

    Update sar.py

--- sar.py
+++ sar.py
@@ -13,17 +13,15 @@ old_p_regex = re.compile(re.escape(old_pattern), re.IGNORECASE)
 for dirpath, dirnames, files in os.walk(topdir, False):
   if '.git' in dirpath:
     continue
-  print('Current dirpath: ' + dirpath)
   for name in files:
     if '.pdf' in name:
       continue
-    print(os.path.join(dirpath, name))
     if old_pattern_for_file_system in name: #rename files in current dir if necessary
       new_file_name = name.replace(old_pattern_for_file_system, new_pattern_for_file_system)
       os.rename(os.path.join(dirpath, name), os.path.join(dirpath, new_file_name))
       name = new_file_name
     
-    f = open(os.path.join(dirpath, name), 'r', encoding='UTF8')
+    f = open(os.path.join(dirpath, name), 'r', encoding='UTF8') #replace file's contents
     filedata = f.read()
     f.close()
 
